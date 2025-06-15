816973caea56c3f4eb6a7c834d78766a15c9e00e
Soul-Scribes <168422977+Soul-Scribes@users.noreply.github.com>
2025-06-15 01:34:14 +0100

    added socials

--- frontend/src/components/Contact.js
+++ frontend/src/components/Contact.js
@@ -1,4 +1,9 @@
 import '../components/styles/Contact.css';
+import instagramIcon from '../assets/icons/instagram-sunsick.png';
+import xIcon from '../assets/icons/x-sunsick.png';
+import youtubeIcon from '../assets/icons/youtube-sunsick.png';
+import facebookIcon from '../assets/icons/facebook-sunsick.png';
+import tiktokIcon from '../assets/icons/tiktok-sunsick.png';
 
 function Contact() {
   return (
@@ -6,11 +11,20 @@ function Contact() {
       <h2>Find Us</h2>
       <p>Follow us on social media for our latest locations and events!</p>
       <div className="social-links">
-        <a href="https://instagram.com/sunsickmoon" target="_blank" rel="noopener noreferrer">
-          Instagram
+        <a href="https://instagram.com/ssmoontruck" target="_blank" rel="noopener noreferrer">
+          <img src={instagramIcon} alt="Instagram" className="social-icon" />
         </a>
-        <a href="https://twitter.com/sunsickmoon" target="_blank" rel="noopener noreferrer">
-          Twitter
+        <a href="https://x.com/ssmoontruck" target="_blank" rel="noopener noreferrer">
+          <img src={xIcon} alt="X" className="social-icon" />
+        </a>
+        <a href="https://youtube.com/@sunsickmoontruck" target="_blank" rel="noopener noreferrer">
+          <img src={youtubeIcon} alt="YouTube" className="social-icon" />
+        </a>
+        <a href="https://facebook.com/ssmoontruck" target="_blank" rel="noopener noreferrer">
+          <img src={facebookIcon} alt="Facebook" className="social-icon" />
+        </a>
+        <a href="https://tiktok.com/@sunsickmoontruck" target="_blank" rel="noopener noreferrer">
+          <img src={tiktokIcon} alt="TikTok" className="social-icon" />
         </a>
       </div>
       <svg className="art-deco-overlay bottom-overlay" viewBox="0 0 100 100" preserveAspectRatio="none">
